ad92c83365f71a4569625b0f2bc387c5b2c0eec2
xiaojian <zhongxiaojian520@qq.com>
2018-10-14 18:50:56 +0800

    更新目录结构

---
+++ client-service/src/main/java/com/xiaojian/clientservice/controller/DcController.java
@@ -0,0 +1,25 @@
+package com.xiaojian.clientservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DcController {
+
+    @Value("${test}")
+    private String test;
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String dc() {
+        String services = "Services: " + discoveryClient.getServices();
+        System.out.println(services+";  "+test);
+        return services+"  我是小坚-PC里面的docker容器服务";
+    }
+
+}
